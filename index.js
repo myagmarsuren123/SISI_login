e8afae570e75d42037666a2ddf314c3b739cc9c1
myagmarsuren123 <58244756+myagmarsuren123@users.noreply.github.com>
2020-06-03 22:22:50 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
-
-const { width, height } = Dimensions.get('window');
-
-class Login extends Component {
-
-}
-export default Login;
